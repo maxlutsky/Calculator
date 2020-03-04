7bb19a398ec24c350f77b3f1687276394029e2cd
Max <gmax115230@gmail.com>
2020-03-04 22:44:10 +0200

    First commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Max on 04/03/2020.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
