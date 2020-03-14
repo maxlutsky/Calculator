97867ca8280a8b8135dcd4b5d87b893dc34ec034
Oleg Boburchak <oleg.boburchak@contractors.roche.com>
2020-03-14 12:48:59 +0100

    add trash

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldOne: UITextField!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+//        showUiImage()
     }
-
-    @IBAction func textField1(_ sender: Any) {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
     }
     
     @IBAction func buttonAction(_ sender: Any) {
         print("Test button")
+        
+        let vc = newVC()
+        
+//        navigationController?.pushViewController(vc, animated: true)
+        
+        
+        
+//        self.presentingViewController(vc)
+    
+//        vc.modalPresentationStyle = .fullScreen
+//        self.present(vc, animated: true, completion: nil)
+        
+        
+//        let vc = UIViewController()
+//        vc.modalPresentationStyle = .fullScreen //or .overFullScreen for transparency
+//        self.present(vc, animated: true, completion: nil)
+        
     }
+    
+//    func showUiImage() {
+//
+//        defer {
+//            print("last element")
+//        }
+//
+//        view.backgroundColor = UIColor.red
+//
+//        var testvalueOptional: String? = "asdasd"
+//
+////        1
+//        testvalueOptional = textFieldOne.text!
+////        print("Test hjkhjkhjkh")
+//
+//
+//        var a: String? = nil
+////        2
+//        guard let somevalue = a else {
+////            print("before return")
+//            return
+////            print("after return")
+//        }
+//        print(somevalue)
+////        print("не виконалось ")
+//
+//
+////        3
+//        if let testvalue = textFieldOne.text {
+//            print(testvalue)
+//        }
+////        print("Test asdasdasd")
+////        4
+//        let noNilValue: String = textFieldOne.text ?? ""
+//
+//    }
+    
+    
+    
+    
 }
 
+
+
+//
+//   https://stackoverflow.com/questions/56435510/presenting-modal-in-ios-13-fullscreen
---
+++ Calculator/newVC.swift
@@ -0,0 +1,22 @@
+//
+//  newVC.swift
+//  Calculator
+//
+//  Created by boburcho on 14/03/2020.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class newVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+    }
+    
+    
+}
